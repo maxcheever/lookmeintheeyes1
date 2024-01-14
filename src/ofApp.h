e53a3a5ebde611f5949601902939b0e4fb26d356
Max Cheever <cheevermax@gmail.com>
2024-01-13 20:43:10 -0500

    deletin stuff

--- src/ofApp.h
+++ src/ofApp.h
@@ -11,31 +11,19 @@ class ofApp : public ofBaseApp{
 		void exit() override;
     
     
-        void makeFaceArr();
-        void makeNewImage();
+        	void makeFaceArr();
+        	void makeNewImage();
 
-		void keyPressed(int key) override;
-		void keyReleased(int key) override;
-		void mouseMoved(int x, int y ) override;
-		void mouseDragged(int x, int y, int button) override;
-		void mousePressed(int x, int y, int button) override;
-		void mouseReleased(int x, int y, int button) override;
-		void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
-		void mouseEntered(int x, int y) override;
-		void mouseExited(int x, int y) override;
 		void windowResized(int w, int h) override;
-		void dragEvent(ofDragInfo dragInfo) override;
-		void gotMessage(ofMessage msg) override;
     
-        struct indexBrightness {
-            int i;
-            float brightness;
-        };
-    
-        ofImage Face;
-        ofImage SampleSrc;
-        ofImage SampleDest;
-        indexBrightness faceArr[786432];
-//        float bIndexDesc[786432];
+	        struct indexBrightness {
+	            int i;
+	            float brightness;
+	        };
+	    
+	        ofImage Face;
+	        ofImage SampleSrc;
+	        ofImage SampleDest;
+	        indexBrightness faceArr[786432];
 		
 };
